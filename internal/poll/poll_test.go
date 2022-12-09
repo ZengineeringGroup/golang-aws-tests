e181655b03a2590b3cec53f2fede4f229476ad59
Christopher Lowenthal <clowenhg@gmail.com>
2022-12-09 13:28:00 -0500

    adds notes

--- internal/poll/poll_test.go
+++ internal/poll/poll_test.go
@@ -15,6 +15,7 @@ import (
 )
 
 func (s *PollSuite) TestPoll() {
+	// Being more specific with the On calls allows us to create different sets of logic for different input
 	call := s.SQS.On("ReceiveMessageWithContext", mock.Anything, mock.Anything, mock.Anything)
 	call.Return(&sqs.ReceiveMessageOutput{}, errors.New("error"))
 
@@ -23,11 +24,15 @@ func (s *PollSuite) TestPoll() {
 	s.Nil(msgs)
 }
 
+// SQSMock only needs mock.Mock and the interface it is mocking, sqsiface.SQSAPI
+// We only need to define the methods that are actually used
 type SQSMock struct {
 	mock.Mock
 	sqsiface.SQSAPI
 }
 
+// This simple mock allows us to override the function behavior within the tests itself
+// using the mock.Call functionality
 func (m *SQSMock) ReceiveMessageWithContext(ctx aws.Context, input *sqs.ReceiveMessageInput, opts ...request.Option) (*sqs.ReceiveMessageOutput, error) {
 	args := m.Called(ctx, input, opts)
 	return args[0].(*sqs.ReceiveMessageOutput), args.Error(1)
@@ -40,7 +45,10 @@ type PollSuite struct {
 }
 
 func (s *PollSuite) SetupSuite() {
+	// Create the mock and use it in our Poller
 	sqsAPI := new(SQSMock)
+
+	// Keep a reference in the Suite itself to avoid the need to case Poller.SQS in tests
 	s.SQS = sqsAPI
 	s.Poller = poll.Poller{
 		SQS: sqsAPI,
